40fe485703e3bf3b46af2b3e4b05eba3ff9d9960
Dima <skydmytro@gmail.com>
2024-07-28 18:13:37 +0200

    style: fix types

--- src/types/fieldTypes.ts
+++ src/types/fieldTypes.ts
@@ -1,3 +1,4 @@
 export type nodeType = { value: 1 | 0; color: 'blue' | 'red' | 'none' };
+export type colorType = 'blue' | 'red' | 'none';
 export type fieldType = nodeType[][];
 export type NodeIdxType = [number, number];
